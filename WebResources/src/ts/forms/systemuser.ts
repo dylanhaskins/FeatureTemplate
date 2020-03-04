151004ceb3bbdf935e87ac0a155f8b0ac223f003
Dylan Haskins <dylan@haskins.co.nz>
2020-03-04 16:05:21 +1300

    Fix SystemUser Sample ts

--- WebResources/src/ts/forms/systemuser.ts
+++ WebResources/src/ts/forms/systemuser.ts
@@ -1,6 +1,10 @@
-﻿function hello(compiler: string) {
-    let BaseForm: Xrm.BasicPage;
-    BaseForm.getAttribute("name").getIsDirty();
-    console.log(`Hello from ${compiler}`);
+﻿
+namespace Demo.Systemuser {
+    var Form: Form.systemuser.Main.User;
+
+    export function onLoad(executionContext: Xrm.ExecutionContext<any,any>) {
+        Form = <Form.systemuser.Main.User>executionContext.getFormContext();
+        // Code here..
+        Form.getAttribute("firstname");
+    }
 }
-hello('TypeScript');
