c311154e039e9ec019fe609ef4b991293e205555
Dylan Haskins <dylan@haskins.co.nz>
2020-03-16 14:00:01 +1300

    Update to _GenerateTypes.ps1

--- Scripts/_GenerateTypes.ps1
+++ Scripts/_GenerateTypes.ps1
@@ -1,4 +1,5 @@
-
+######################## SETUP 
+. (Join-Path $PSScriptRoot "_Config.ps1")
 
 if (!$Credentials)
 {
@@ -34,4 +35,38 @@ Set-Location -Path (Join-Path $PSScriptRoot "..\XrmContext")
 Set-Location -Path (Join-Path $PSScriptRoot "..\XrmDefinitelyTyped")
 . .\XrmDefinitelyTyped.exe /url:$global:ServerUrl/XRMServices/2011/Organization.svc /username:$username /password:$password /useconfig /out:"../Webresources/typings/XRM" /jsLib:"../Webresources/src/library"
 Set-Location -Path $CurrentLocation
+##Add Files to Project
+[xml]$xdoc = (Get-Content (Join-Path $PSScriptRoot "..\AddName.csproj"))
+
+[System.Xml.XmlNamespaceManager] $nsmgr = $xdoc.NameTable
+$nsmgr.AddNamespace('a','http://schemas.microsoft.com/developer/msbuild/2003')
+
+$nodes = $xdoc.SelectNodes("//a:Compile[contains(@Include,'Entities\Context')]",$nsmgr)
+for ($i=0; $i -le ($nodes.Count-1); $i++)
+        {
+            $nodes[$i].ParentNode.RemoveChild($nodes[$i])
+        }
+
+$newnodes = $xdoc.SelectNodes("//a:Compile",$nsmgr)
+$addNode = $newnodes[0].Clone()
+
+Get-ChildItem (Join-Path $PSScriptRoot "..\Entities\Context") -Name | ForEach-Object {
+	$newnodes = $xdoc.SelectNodes("//a:Compile",$nsmgr)
+    $addNode = $newnodes[0].Clone()
+	$addNode.Include = "Entities\Context\$_"; $newnodes[0].ParentNode.AppendChild($addNode)
+}
+
+$nodes = $xdoc.SelectNodes("//a:TypeScriptCompile[contains(@Include,'WebResources\typings\XRM')]",$nsmgr)
+for ($i=0; $i -le ($nodes.Count-1); $i++)
+        {
+            $nodes[$i].ParentNode.RemoveChild($nodes[$i])
+        }
+
+Get-ChildItem (Join-Path $PSScriptRoot "..\WebResources\typings\XRM") -Name -Recurse | ForEach-Object {
+	$newnodes = $xdoc.SelectNodes("//a:TypeScriptCompile",$nsmgr)
+    $addNode = $newnodes[0].Clone()
+	$addNode.Include = "WebResources\typings\XRM\$_"; $newnodes[0].ParentNode.AppendChild($addNode)
+}
+
+$xdoc.Save((Join-Path $PSScriptRoot "..\AddName.csproj"))
 }
