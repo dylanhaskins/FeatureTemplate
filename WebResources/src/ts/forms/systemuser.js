1214fcf986cae0f7cbbc30ee17629c326f498de4
Dylan Haskins <dylan@haskins.co.nz>
2020-03-19 00:06:45 +1300

    Fixes for TypeScript

--- WebResources/src/ts/forms/systemuser.js
+++
@@ -1,7 +0,0 @@
-function hello(compiler) {
-    //let BaseForm: Xrm.BasicPage;
-    //BaseForm.getAttribute("name").getIsDirty();
-    console.log("Hello from " + compiler);
-}
-hello('TypeScript');
-//# sourceMappingURL=systemuser.js.map
